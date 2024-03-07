91fe7b8f8e1c6ceb3e9bb6ba2b9f65807b9925a4
inux39 <inux39@protonmail.ch>
2024-03-07 11:43:18 +0900

    fix vpn.bash

--- bin/vpn.bash
+++ bin/vpn.bash
@@ -7,6 +7,11 @@ function SUDO() {
     fi
 }
 
-SUDO ip route add 192.168.0.0/16 via 192.168.10.1
 SUDO ip route
+VIA="192.168.80.1"
+echo -n "route add via $VIA [y/n]:"
+read CONTINUE
+if [ $CONTINUE = "y" ]; then
+    SUDO ip route add 192.168.0.0/16 via $VIA
+fi
 
